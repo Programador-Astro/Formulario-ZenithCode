603edea7f9b1c5ea4750214564bd55141897d3a6
Programador-Astro <programadorastro@gmail.com>
2025-04-25 22:53:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,6 @@ def integrantes():
     usuarios = Usuario.query.all()
     return render_template('integrantes.html', usuarios=usuarios)
 
-Flask.run(app, debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)
